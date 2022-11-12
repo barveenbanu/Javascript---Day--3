536f789c68b5a85342cfad9a48a1dabd312f8c85
barveenbanu <111632582+barveenbanu@users.noreply.github.com>
2022-11-13 01:45:09 +0530

    Add files via upload

---
+++ resume-json-object.js
@@ -0,0 +1,18 @@
+	const obj = {
+  "name": "Banu",
+  "age": 30,
+  "dob": "1988-12-19",
+  
+  "skills": [
+    "JS",
+    "C++"
+  ],
+	}
+	
+for (x in obj)
+		{
+			//console.log(x + " : " + obj[x])
+			console.log(`${x} : ${obj[x]}`)// template literals
+		}
+		
+		
